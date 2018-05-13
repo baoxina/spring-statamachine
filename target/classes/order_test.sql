40856157ae96c00afadcea425b0bd98a4cc7ae7c
包鑫 <bx19920704@sina.com>
2018-05-13 21:46:28 +0800

    Delete order_test.sql

--- target/classes/order_test.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE
-    order_test
-    (
-        id BIGINT NOT NULL,
-        order_id VARCHAR(50) NOT NULL,
-        status INT(11) NOT NULL,
-        PRIMARY KEY (id)
-    )
-    ENGINE=InnoDB DEFAULT CHARSET=utf8
