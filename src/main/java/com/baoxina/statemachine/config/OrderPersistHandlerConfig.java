4afd59d0ab09acd840bc9aaf809add82d785f46f
baoxina <baoxin@cailian.net>
2018-05-14 15:57:57 +0800

    11
    Signed-off-by: baoxina <baoxin@cailian.net>

--- src/main/java/com/baoxina/statemachine/config/OrderPersistHandlerConfig.java
+++ src/main/java/com/baoxina/statemachine/config/OrderPersistHandlerConfig.java
@@ -1,7 +1,5 @@
 package com.baoxina.statemachine.config;
 
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -11,25 +9,14 @@ import org.springframework.statemachine.StateMachine;
 import com.baoxina.statemachine.PersistStateMachineHandler;
 import com.baoxina.statemachine.entity.OrderEvents;
 import com.baoxina.statemachine.entity.OrderStatus;
-import com.baoxina.statemachine.service.OrderStateService;
 
 @Configuration
 public class OrderPersistHandlerConfig {
 
-	@Resource(name="orderPersistStateChangeListener")
-	private OrderPersistStateChangeListener orderPersistStateChangeListener;
 	
     @Autowired
     @Qualifier("orderStateMachine")
     private StateMachine<OrderStatus, OrderEvents> orderStateMachine;
-
-
-    @Bean
-    public OrderStateService orderStateService() {
-        PersistStateMachineHandler handler = persistStateMachineHandler();
-        handler.addPersistStateChangeListener(orderPersistStateChangeListener);
-        return new OrderStateService(handler);
-    }
 
     @Bean
     public PersistStateMachineHandler persistStateMachineHandler() {
--- src/main/java/com/baoxina/statemachine/service/OrderStateService.java
+++ src/main/java/com/baoxina/statemachine/service/OrderStateService.java
@@ -3,27 +3,40 @@ package com.baoxina.statemachine.service;
 import java.util.List;
 import java.util.StringJoiner;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.baoxina.statemachine.PersistStateMachineHandler;
+import com.baoxina.statemachine.config.OrderPersistStateChangeListener;
 import com.baoxina.statemachine.dao.OrderRepo;
 import com.baoxina.statemachine.entity.Order;
 import com.baoxina.statemachine.entity.OrderEvents;
 
+@Component
+@Transactional
 public class OrderStateService {
 
-    private PersistStateMachineHandler handler;
+	@Resource(name="orderPersistStateChangeListener")
+	private OrderPersistStateChangeListener orderPersistStateChangeListener;
+	
+	@Resource(name="persistStateMachineHandler")
+    private PersistStateMachineHandler persistStateMachineHandler;
 
-
-    public OrderStateService(PersistStateMachineHandler handler) {
-        this.handler = handler;
+	
+	@PostConstruct
+    private void initialize() {
+		this.persistStateMachineHandler.addPersistStateChangeListener(orderPersistStateChangeListener);
     }
 
     @Autowired
     private OrderRepo repo;
 
-
+    @Transactional(readOnly=true)
     public String listDbEntries() {
         List<Order> orders = repo.findAll();
         StringJoiner sj = new StringJoiner(",");
@@ -33,10 +46,10 @@ public class OrderStateService {
         return sj.toString();
     }
 
-
+    @Transactional(readOnly=false)
     public boolean change(int order, OrderEvents event) {
         Order o = repo.findByOrderId(order);
-        return handler.handleEventWithState(MessageBuilder.withPayload(event).setHeader("order", order).build(), o.getStatus());
+        return persistStateMachineHandler.handleEventWithState(MessageBuilder.withPayload(event).setHeader("order", order).build(), o.getStatus());
     }
 
 }
